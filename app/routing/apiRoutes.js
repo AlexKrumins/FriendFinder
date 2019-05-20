88c07f3c5c774835e858691b89eeb63f2aa74335
Alex Krumins <justkrumins@gmail.com>
2019-05-19 22:54:08 -0500

    unable to use database and logic in conjunction

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -24,8 +24,7 @@ app.get("/api/friends", function(req, res) {
     return res.json(data);
   });
 });
-app.post("/api/friends", function(req, res) {
-  console.log(req.body.scores)
+app.post("/api/friends", function(req, res, next) {
   connection.query("INSERT INTO friends_db.friends (friend, photo, scores) VALUES (?,?,?)", [req.body.friend, req.body.photo,'['+req.body.scores+']'], function(err, result) {
     if (err) {
       return res.status(500).end();
@@ -33,6 +32,38 @@ app.post("/api/friends", function(req, res) {
     // Send back the ID of the new friend
     res.json({ id: result.insertId });
     console.log({ id: result.insertId });
-  });
-});
-};
+  next()
+}, function(req, res) {
+    var userProfile = req.body;
+    for (var i = 0; i < userProfile.scores.length; i++) {
+      userProfile.scores[i] = parseInt(userProfile.scores[i]);
+    }
+    connection.query("SELECT * FROM friends", function(err, data) {
+      if(err) {
+        return res.status(500).end();
+      }
+      return res.json(data);
+    })
+    //This will be the array position of the best matched friend
+    var allFriendsIndex = 0;
+    var maxDifference = 40;
+    //Getting all friends from the database
+
+    var allFriends = res.json(data);
+    //loop through the database and compare the scores
+    for(var n=0; n < allFriends.length; n++) {
+      var totalDifference = 0;
+      for (var h = 0; h<allFreinds[n].scores.length; h++) {
+        var scoreDifference = Math.abs(userProfile.scores[h] - allFriends[n].scores[h]);
+        totalDifference += scoreDifference;
+      }
+      //this will determine if the current friend in the loop is better that the current placeholder
+      if(totalDifference < maxDifference) {
+        allFriendsIndex = n;
+        maxDifference = totalDifference;
+      }
+    }
+    res.json(allFriends[allFriendsIndex]);
+  })
+})
+}
