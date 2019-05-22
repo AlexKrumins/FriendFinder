5f20917d9ddfa8c04478a13d207a5da95f52d389
Alex Krumins <justkrumins@gmail.com>
2019-05-22 16:32:50 -0500

    connections to external db finally working

--- app/db/seeds.sql
+++ app/db/seeds.sql
@@ -2,5 +2,23 @@ INSERT INTO friends (friend, photo, scores)
 VALUES (
     'Susan',
     'https://en.wikiquote.org/wiki/Susan_Sarandon#/media/File:Susan_Sarandon_3_by_David_Shankbone.jpg',
-    '[5,5,5,5,5,5,5,5,5,5]'
+    '5 5 5 5 5 5 5 5 5 5'
+);
+INSERT INTO friends (friend, photo, scores)
+VALUES (
+    'Ed',
+    'https://pbs.twimg.com/profile_images/648888480974508032/66_cUYfj_400x400.jpg',
+    '3 3 1 5 4 1 4 1 2 3'
+);
+INSERT INTO friends (friend, photo, scores)
+VALUES (
+    'Viggo',
+    'https://m.media-amazon.com/images/M/MV5BNDQzOTg4NzA2Nl5BMl5BanBnXkFtZTcwMzkwNjkxMg@@._V1_UX214_CR0,0,214,317_AL_.jpg',
+    '2 1 4 4 5 2 1 2 1 3'
+);
+INSERT INTO friends (friend, photo, scores)
+VALUES (
+    'Chester',
+    'https://pbs.twimg.com/profile_images/1092205794047680512/KmXvl9Is.jpg',
+    '1 4 5 1 5 1 1 1 5 1'
 );
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,19 +1,13 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "k3xio06abqa902qt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "",
-  database: "heroku_bf0299f0896d543"
+  user: "qrs9yep1ere6lkdt",
+  password: "ojf5wsew0qpktqm8",
+  database: "yg2x9f6m2g2eszyl"
 });
-// var connection = mysql.createConnection({
-//   host: "us-cdbr-iron-east-02.cleardb.net",
-//   port: 3306,
-//   user: "b5cc49ce32b600",
-//   password: "480566a2c06cf2b",
-//   database: "heroku_bf0299f0896d543"
-// });
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -31,46 +25,40 @@ app.get("/api/friends", function(req, res) {
     return res.json(data);
   });
 });
-app.post("/api/friends", function(req, res, next) {
-  connection.query("INSERT INTO friends (friend, photo, scores) VALUES (?,?,?)", [req.body.friend, req.body.photo,'['+req.body.scores+']'], function(err, result) {
-    if (err) {
+app.post("/api/friends", function(req, res) {
+  connection.query("SELECT * FROM friends", function(err, data) {
+    if(err) {
       return res.status(500).end();
     }
-    // Send back the ID of the new friend
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
-  next()
-}, function(req, res) {
     var userProfile = req.body;
-    for (var i = 0; i < userProfile.scores.length; i++) {
-      userProfile.scores[i] = parseInt(userProfile.scores[i]);
-    }
-    connection.query("SELECT * FROM friends", function(err, data) {
-      if(err) {
-        return res.status(500).end();
-      }
-      return res.json(data);
-    })
     //This will be the array position of the best matched friend
     var allFriendsIndex = 0;
     var maxDifference = 40;
     //Getting all friends from the database
-
-    var allFriends = res.json(data);
+    var allFriends = data;
     //loop through the database and compare the scores
-    for(var n=0; n < allFriends.length; n++) {
+    for(var j = 0; j < allFriends.length; j++) {
       var totalDifference = 0;
-      for (var h = 0; h<allFreinds[n].scores.length; h++) {
-        var scoreDifference = Math.abs(userProfile.scores[h] - allFriends[n].scores[h]);
+      var arr = allFriends[j].scores.split(" ")
+      // console.log("allFriends.scores: " + allFriends[j].scores)
+      // console.log("res: " + res);
+      for (var h = 0; h < arr.length; h++) {
+        // console.log("user score at " + h + " = " + userProfile.scores[h])
+        var scoreDifference = Math.abs(parseInt(userProfile.scores[h]) - parseInt(arr[h]));
+        // console.log("scoreDifference: " + scoreDifference)
         totalDifference += scoreDifference;
       }
+      // console.log("Name " + allFriends[j].friend)
+      // console.log("total difference " + totalDifference);
       //this will determine if the current friend in the loop is better that the current placeholder
       if(totalDifference < maxDifference) {
-        allFriendsIndex = n;
+        allFriendsIndex = j;
         maxDifference = totalDifference;
       }
     }
-    res.json(allFriends[allFriendsIndex]);
+    // console.log(userProfile.scores.join(" "));
+    connection.query("INSERT INTO friends (friend, photo, scores) VALUES (?,?,?)", [userProfile.friend, userProfile.photo, userProfile.scores.join(" ")])
+    // console.log(allFriends[allFriendsIndex]);
+    return res.json(allFriends[allFriendsIndex]);
   })
-})
-}
+})}
